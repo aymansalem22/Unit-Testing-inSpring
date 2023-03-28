c554584fa356e621d466654a802f51f3dff6c571
aymansalem22 <s.aymansalem@gmail.com>
2023-03-29 01:18:16 +0200

    update

--- src/main/java/com/paltecno/unittesting/ArrayMethods.java
+++ src/main/java/com/paltecno/unittesting/ArrayMethods.java
@@ -1,24 +1,35 @@
 package com.paltecno.unittesting;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ArrayMethods {
 
 	int findIndex(int[] array, int number) {
-		int index=-1;
-		for(int i=0;i<array.length;i++) {
-			if(array[i]==number)
-				index=i;
+		int index = -1;
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == number)
+				index = i;
 		}
 		return index;
 	}
-	
-	boolean searchNumber(int[] array,int numberToSearchFor) {
-		boolean found=false;
-		for(int i:array) {
-			if(array[i]==numberToSearchFor) {
-				found=true;
+
+	boolean searchNumber(int[] array, int numberToSearchFor) {
+		boolean found = false;
+		for (int i : array) {
+			if (array[i] == numberToSearchFor) {
+				found = true;
 				return found;
 			}
 		}
 		return found;
+	}
+
+	int[] sortArray(int[] numbers) {
+		return new int[] {};
+	}
+
+	void printArray(int[] array, int index) {
+		System.out.println(array[index]);
 	}
 }
--- src/test/java/com/paltecno/unittesting/ArrayMethodsTest.java
+++ src/test/java/com/paltecno/unittesting/ArrayMethodsTest.java
@@ -8,21 +8,63 @@ class ArrayMethodsTest {
 
 	@Test
 	public void testFindIndex_numberInArray() {
-	ArrayMethods arrayMethods=new ArrayMethods();
-	int result=arrayMethods.findIndex(new int[] {8,4,5}, 4);
-	assertEquals(1, result);
+		ArrayMethods arrayMethods = new ArrayMethods();
+		int result = arrayMethods.findIndex(new int[] { 8, 4, 5 }, 4);
+		assertEquals(1, result);
 	}
 
 	@Test
 	public void testFindIndex_numberNotInArray() {
-		ArrayMethods arrayMethods=new ArrayMethods();
-		assertEquals(-1, arrayMethods.findIndex(new int[] {8,4,5}, 1));
+		ArrayMethods arrayMethods = new ArrayMethods();
+		assertEquals(-1, arrayMethods.findIndex(new int[] { 8, 4, 5 }, 1));
 	}
-	
+
 	@Test
 	public void testfindIndex_emptyArray() {
-		ArrayMethods arrayMethods=new ArrayMethods();
+		ArrayMethods arrayMethods = new ArrayMethods();
 		assertEquals(-1, arrayMethods.findIndex(new int[] {}, 1));
+	}
+
+	@Test
+	public void testFindIndex_numberNotInArraywithmessage() {
+		ArrayMethods arrayMethods = new ArrayMethods();
+		assertEquals(1, arrayMethods.findIndex(new int[] { 8, 4, 5 }, 1),
+				"The findIndex method finds the index of a given number");
+	}
+
+	@Test
+	public void testassertArrayEqual() {
+		assertArrayEquals(new int[] { 0, 1, 3, 7 }, new int[] { 0, 5, 3, 7 }, "both arent equal!");
+	}
+
+	@Test
+	void testAssert() {
+		Boolean condition = true;
+		assertEquals(true, true);
+		assertTrue(condition);
+
+		String str = null;
+		assertEquals(null, str);
+		assertNull(str);
+	}
+
+	@Test
+	public void testSortArray() {
+		fail("unimplemented method");
+	}
+
+	@Test
+	public void testfindIndex() {
+		ArrayMethods arrayMethods = new ArrayMethods();
+		assertAll(() -> assertEquals(1, arrayMethods.findIndex(new int[] { 8, 4, 5 }, 4)),
+				() -> assertEquals(-1, arrayMethods.findIndex(new int[] { 8, 4, 5 }, 1)),
+				() -> assertEquals(-1, arrayMethods.findIndex(new int[] {}, 1)));
+	}
+
+	@Test
+	public void testfindIndex_indexOutOfBound() {
+		ArrayMethods arrayMethods = new ArrayMethods();
+		assertThrows(ArrayIndexOutOfBoundsException.class, () -> arrayMethods.printArray(new int[] { 1, 8, 5 }, 3));
 	}
 
 }
