9ea2387a105afd38389d8934042e11ba27a31338
aymansalem22 <s.aymansalem@gmail.com>
2023-03-29 03:09:58 +0200

    Mockito Annotations

---
+++ src/test/java/com/paltecno/unittesting/MockitoAnnotationsTesting.java
@@ -0,0 +1,33 @@
+package com.paltecno.unittesting;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class MockitoAnnotationsTesting {
+
+	@Mock
+	StudentDao studentDaoMock;
+
+	@InjectMocks
+	StudentService studentService;
+
+	@Test
+	void testFindTotal() {
+		when(studentDaoMock.getMarks()).thenReturn(new int[] { 15, 20, 5 });
+		assertEquals(40, studentService.findTotal());
+	}
+
+	@Test
+	void testFindTotal_EmptyArray() {
+		when(studentDaoMock.getMarks()).thenReturn(new int[] {});
+		assertEquals(0, studentService.findTotal());
+	}
+
+}
